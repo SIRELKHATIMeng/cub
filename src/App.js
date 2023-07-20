c0de2e45d05f143a956d54ac4bcf51ce729fe0dc
SIRELKHATIMeng <sabdraouf4@gmail.com>
2023-07-21 01:03:27 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+ ** Author: Santosh Kumar Dash
+ ** Author URL: http://santoshdash.epizy.com/
+ ** Github URL: https://github.com/quintuslabs/fashion-cube
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./assets/css/style.css";
+import "./assets/css/responsive.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+//import Routes from "./routes";
+import { Switch } from "react-router-dom/cjs/react-router-dom.min";
+import { BrowserRouter,Route, Router } from "react-router-dom";
+//import { Routes } from "react-router-dom";
+import Home from "./views/Home/Home";
+import BaseLayout from "./layouts/BaseLayout";
+import Routes from './routes'
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  componentDidMount() {
+    AOS.init();
+  }
+  render() {
+    return (
+     <div>
+      
+       <Routes/>
+       
+      </div>
+    );
+  }
 }
 
 export default App;
